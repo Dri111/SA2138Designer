09d2e3c912b8a62ec7a5ad901858acdb80cca737
Dri111 <leeyuchan2004@gmail.com>
2023-01-17 21:09:15 +0900

    Added Root Builder Model && Rotation System(not work yet)

--- app.js
+++ app.js
@@ -10,16 +10,21 @@ function randomName() {
  * For the ports
  * will be recieved via the position information of owner part.
  * vector v: The relative position of port according to G
- * vector u: The direction vector of port outwards 
- * @param {THREE.Vector3} G
+ * vector u: The direction vector of port outwards
+ * vector r: The rotation vector, which indicates the direction of 0 angle 
  * @param {THREE.Vector3} u
  * @param {THREE.Vector3} v
+ * @param {THREE.Vector3}
  */
-function Port(v, u) {
+function Port(v, u, r) {
     return {
         u: u,
-        v: v
+        v: v,
+        r: r
     }
+}
+function isParallelTo(v1, v2) {
+    return v1.clone().normalize().equals(v2.clone().normalize())
 }
 function v3(x, y, z) {
     return new THREE.Vector3(x, y, z)
@@ -30,21 +35,61 @@ function init() {
     let objLoader = new THREE.OBJLoader();
     let mtlLoader = new THREE.MTLLoader();
     const _q1 = new THREE.Quaternion();
+    let RootPart;
+    let partLoadingN = 3;
+
+    let $$$ = {
+        W: window.innerWidth,
+        H: window.innerHeight * 0.9,
+        partN: 0,
+        selectedName: "",
+        selectedModule: "Hub",
+        selectedPortID: 0,
+        rotation: 0,
+        part: {},
+        tree: (part) => {
+            return
+        },
+        nodePart: null,
+        attachNodeList: { root_0: null },
+        attachNodeArray: [],
+        addNode: (node) => {
+            $$$.attachNodeList[node.name] = node;
+            $$$.attachNodeArray.push(node);
+        },
+        connectMod(m1, m2, p1ind, p2ind) {
+            m1.connected[p1ind] = m2;
+            m2.connected[p2ind] = m1;
+            m1.port[p1ind].connect(m2.port[p2ind])
+            m2.port[p2ind].connect(m1.port[p1ind])
+        }
+    }
+
+
 
     class Part {
         static partList = {
+            RootPartBuilder: {
+                name: "RootPartBuilder",
+                G: "./RootPart.obj",
+                M: "./RootPart.mtl",
+                portAmount: 1,
+                portPD: [
+                    Port(v3(1.26919, 0, 0), v3(1, 0, 0), v3(0, 1, 0))
+                ]
+            },
             Hub: {
                 name: "Hub",
                 G: "./Hub.obj",
                 M: "./Hub.mtl",
                 portAmount: 6,
                 portPD: [
-                    Port(v3(-0.5, 0, 0), v3(-1, 0, 0)),
-                    Port(v3(0, -0.5, 0), v3(0, -1, 0)),
-                    Port(v3(0, 0, -0.5), v3(0, 0, -1)),
-                    Port(v3(0.5, 0, 0), v3(1, 0, 0)),
-                    Port(v3(0, 0.5, 0), v3(0, 1, 0)),
-                    Port(v3(0, 0, 0.5), v3(0, 0, 1))
+                    Port(v3(-0.5, 0, 0), v3(-1, 0, 0), v3(0, -1, 0)),
+                    Port(v3(0.5, 0, 0), v3(1, 0, 0), v3(0, -1, 0)),
+                    Port(v3(0, 0, -0.5), v3(0, 0, -1), v3(-1, 0, 0)),
+                    Port(v3(0, 0, 0.5), v3(0, 0, 1), v3(-1, 0, 0)),
+                    Port(v3(0, 0.5, 0), v3(0, 1, 0), v3(-1, 0, 0)),
+                    Port(v3(0, -0.5, 0), v3(0, -1, 0), v3(-1, 0, 0))
                 ],
             },
             Corridor: {
@@ -53,8 +98,8 @@ function init() {
                 M: "./Station_Module.mtl",
                 portAmount: 2,
                 portPD: [
-                    Port(v3(-0.581891, 0, 0), v3(-1, 0, 0)),
-                    Port(v3(0.581891, 0, 0), v3(1, 0, 0)),
+                    Port(v3(-1.12936, 0, 0), v3(-1, 0, 0), v3(0, -1, 0)),
+                    Port(v3(1.12936, 0, 0), v3(1, 0, 0), v3(0, -1, 0)),
                 ],
             }
         }
@@ -65,10 +110,18 @@ function init() {
                 objLoader.setMaterials(material);
                 objLoader.setPath("./asset/");
                 objLoader.load(Part.partList[name].G, (object) => {
-                    Part.partList[name].mesh=object.children[0].clone();
+                    Part.partList[name].mesh = object.children[0].clone();
+                    partLoadingN--;
+                    if (partLoadingN == 0) {
+                        RootPart = new Part("RootPartBuilder", null, "root");
+                        $$$.attachNodeList.root_0 = RootPart.port[0];
+                        $$$.attachNodeArray.push(RootPart.port[0]);
+                        RootPart.port[0].makeRoot();
+                    }
                 },
                     (xhr) => {
                         console.log(`Loading OBJ of ${name}`, xhr);
+
                     },
                     (error) => {
                         console.error("Error has Occured : ", error);
@@ -103,15 +156,14 @@ function init() {
             this.partInfo = Part.partList[partName];
             this.connected = [];
             this.name = name;
+            this.visited = false;
             this.port = [];
             scene.add(this.mesh);
             for (let i = 0; i < Part.partList[partName].portPD.length; i++) {
                 this.connected.push(-1);
                 this.port[i] = new AttachNode(this, scene, i);
                 $$$.addNode(this.port[i]);
-                if (this.name != "root") {
-                    this.port[i].visualize();
-                }
+
             }
 
         }
@@ -130,6 +182,11 @@ function init() {
                 }
             }*/
         }
+        visualize() {
+            for (const port of this.port) {
+                port.visualize()
+            }
+        }
     }
     Part.init();
     class AttachNode extends THREE.Mesh {
@@ -142,6 +199,7 @@ function init() {
             this.portInfo = part.partInfo.portPD[portID]
             this.v = this.portInfo.v.clone();
             this.u = this.portInfo.u.clone();
+            this.r = this.portInfo.r.clone();
             this.selected = 0;
             this.isAttachNode = 1;
             this.isOccupied = false;
@@ -159,13 +217,12 @@ function init() {
             return new THREE.Color(`hsl(${this.pid * 60},100%,50%)`)
         }
         visualize() {
-            let uviewer = new THREE.ArrowHelper(this.u, this.position, 1);
+            let uviewer = new THREE.ArrowHelper(this.u, this.position, 0.5);
             uviewer.setColor(this.calcColor())
             scene.add(uviewer);
-            console.log(this.v, this)
-            let vviewer = new THREE.ArrowHelper(this.v.clone().normalize(), this.part.position, this.v.length())
-            vviewer.setColor(this.calcColor())
-            scene.add(vviewer);
+            let rviewer = new THREE.ArrowHelper(this.r.clone(), this.position, 1);
+            rviewer.setColor(new THREE.Color("white"))
+            scene.add(rviewer)
         }
         updateMat() {
             //this.visible=false;
@@ -208,6 +265,7 @@ function init() {
         rotate(quaternion) {
             this.v.applyQuaternion(quaternion);
             this.u.applyQuaternion(quaternion);
+            this.r.applyQuaternion(quaternion);
             this.position.addVectors(this.part.mesh.position, this.v)
             for (let i = 0; i < 3; i++) {
                 let v = this.position.getComponent(i)
@@ -218,64 +276,59 @@ function init() {
         }
     }
 
-    class RootPart extends Part {
-        static position = v3(0, 0, 0)
-        static rotation = v3(0, 0, 0)
-        static name = "root"
-        static partInfo = {
-            name: "root",
-            G: null,
-            M: null,
-            portPD: [Port(v3(0, 0, 0), v3(1, 0, 0))]
-        }
-        static mesh={
-            position:v3(0,0,0)
-        }
-        static port = new AttachNode(this, scene, 0)
-    }
+
 
 
     let $UI = {
         idSelectionBox: document.getElementById('idsel-box'),
         button: {
-            module_select: []
+            moduleSelect: {},
+            blueprint: document.querySelector('button#button-blueprint')
+        },
+        range: {
+            rotation: document.querySelector('input#angle-range')
+        },
+        text: {
+            rotation: document.querySelector('span#angle-text')
         },
         init: () => {
+            $UI.range.rotation.value = 0;
+            $UI.text.rotation.innerText = `0˚`
             for (const name of Object.keys(Part.partList)) {
-                let dom = document.getElementById(`select-${name.toLowerCase()}`);
-                $UI.button.module_select.push(dom);
-                dom.addEventListener('click', (e) => {
-                    $$$.selectedModule = name;
-                    updateIDButton();
-                    console.log(name);
-                })
+                if (name != "RootPartBuilder") {
+                    let dom = document.getElementById(`select-${name.toLowerCase()}`);
+                    $UI.button.moduleSelect[name] = dom;
+                    dom.addEventListener('click', (e) => {
+                        $UI.updateModuleButtonUI(name);
+                        $$$.selectedModule = name;
+                        updateIDButton();
+                        console.log(name);
+                    })
+                }
+            }
+            $UI.range.rotation.addEventListener('input', () => {
+                $$$.rotation = $UI.range.rotation.value;
+                $UI.text.rotation.innerText = `${$$$.rotation}˚`
+            })
+            $UI.button.blueprint.addEventListener('click', () => {
+
+            })
+        },
+        selectButtonID: () => {
+            //Nothing. yea nothing
+        },
+        updateModuleButtonUI: (nname) => {
+            if (nname != $$$.selectedModule) {
+                $UI.button.moduleSelect[nname].state = "selected";
+                $UI.button.moduleSelect[nname].style.border = '3px solid red';
+                $UI.button.moduleSelect[$$$.selectedModule].state = "none";
+                $UI.button.moduleSelect[$$$.selectedModule].style.border = 'none';
             }
         }
     }
     $UI.init();
-    let $$$ = {
-        W: window.innerWidth,
-        H: window.innerHeight * 0.9,
-        partN: 0,
-        selectedName: "",
-        selectedModule: "Hub",
-        selectedPortID: 0,
-        part: {},
-        nodePart: null,
-        attachNodeList: { root_0: RootPart.port },
-        attachNodeArray: [RootPart.port],
-        addNode: (node) => {
-            $$$.attachNodeList[node.name] = node;
-            $$$.attachNodeArray.push(node);
-        },
-        connectMod(m1, m2, p1ind, p2ind) {
-            m1.connected[p1ind] = m2;
-            m2.connected[p2ind] = m1;
-            m1.port[p1ind].connect(m2.port[p2ind])
-            m2.port[p2ind].connect(m1.port[p1ind])
-        }
-    }
-    RootPart.port.makeRoot();
+
+
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(new THREE.Color(0x000000));
@@ -286,12 +339,12 @@ function init() {
     camera.lookAt(scene.position);
 
 
-    let controls = new THREE.TrackballControls( camera, renderer.domElement );
+    let controls = new THREE.TrackballControls(camera, renderer.domElement);
 
     controls.rotateSpeed = 1.0;
     controls.zoomSpeed = 1.2;
     controls.panSpeed = 0.8;
-    controls.staticMoving=true;
+    controls.staticMoving = true;
     var clock = new THREE.Clock();
     function render() {
         var delta = clock.getDelta();
@@ -332,10 +385,7 @@ function init() {
     var clickEvent = (e) => {
         if ($$$.selectedName != "" && $$$.attachNodeList[$$$.selectedName].selected == 1) {
             let npart = new Part($$$.selectedModule, $$$.attachNodeList[$$$.selectedName]);
-            if ($$$.attachNodeList[$$$.selectedName].isRoot) {
-                scene.add(npart.mesh);
-            }
-            else {
+
                 //Original port
                 let originalPort = $$$.attachNodeList[$$$.selectedName]
                 //new port
@@ -344,7 +394,7 @@ function init() {
                 let u1 = originalPort.u.clone();
                 let u2 = newPort.u.clone();
                 let rotA = u1.clone().cross(u2);
-                console.log(originalPort.pid)
+                //console.log(originalPort.pid)
                 if (!(rotA.x == 0 && rotA.y == 0 && rotA.z == 0)) {
                     u1 = u1.negate().angleTo(u2);
                     _q1.setFromAxisAngle(rotA, u1);
@@ -356,18 +406,32 @@ function init() {
                     npart.rotate(_q1);
                     npart.mesh.applyQuaternion(_q1)
                 }
+
+                npart.recalibrate();
+                u1 = originalPort.r.angleTo(newPort.r);
+                //console.log(u1)
+                if (isParallelTo(originalPort.r, newPort.r)) {
+                    rotA = newPort.u
+                } else {
+                    rotA = originalPort.r.clone().cross(newPort.r);
+                }
+                u1 = Math.PI - u1 + $$$.rotation
+                _q1.setFromAxisAngle(rotA, u1);
+                npart.rotate(_q1);
+                npart.mesh.applyQuaternion(_q1);
                 //originalPort.position+newPort.v.negate();
                 npart.mesh.position.addVectors(originalPort.position, newPort.v.clone().negate());
                 npart.recalibrate();
+                npart.visualize();
                 $$$.connectMod(originalPort.part, npart, originalPort.pid, $$$.selectedPortID);
-            }
+            
         }
     }
     function updateIDButton() {
         $UI.idSelectionBox.replaceChildren();
         let n = Part.partList[$$$.selectedModule].portAmount;
         let txt = document.createElement("td");
-        txt.innerText = `Selected ID: ${$$$.selectedPortID} Selected Module:${$$$.selectedModule}`
+        txt.innerText = `Selected ID: ${$$$.selectedPortID}`
         for (let i = 0; i < n; i++) {
             let td = document.createElement('td');
             let btn = document.createElement('button');
